f5789975031a07387ec25bcf8115bfdeb9e08a8e
eborghi10 <emiliano_borghi@hotmail.com>
2017-01-23 11:10:25 -0300

    first commit

---
+++ JavaBasics/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+	// instance variables
+        int num = 0;
+        static String name;
+
+    }
+}
